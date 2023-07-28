4d3572c5c13c6b392924bc0cd5c9b68e9a2823eb
Krzysztof Trela <krzysztof.trela77@gmail.com>
2023-07-28 16:29:12 +0200

    Liczby

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//var myAge = 33;
+
+int myAge2 = 120;
+int newAge = myAge2 + 5;
+Console.WriteLine(newAge);
+int myVar = int.MinValue;
+uint myVar2 = uint.MaxValue;
+ulong myVar3 = ulong.MaxValue;
+
