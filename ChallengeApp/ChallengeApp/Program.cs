6621cc84904a0e07ce3f3ddc7636c47aae4e2140
Krzysztof Trela <krzysztof.trela77@gmail.com>
2023-07-31 17:56:59 +0200

    Metody, klasy itd

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,71 +1,47 @@
-﻿int number = 4566;
-int zero = 0;
-int one = 0;
-int two = 0;
-int three = 0;
-int four = 0;
-int five = 0;
-int six = 0;
-int seven = 0;
-int eight = 0;
-int nine = 0;
+﻿using System.Threading.Tasks.Sources;
 
-string numberInString = number.ToString();
-char[] letters = numberInString.ToCharArray();
+User user1 = new User("Adam", "Adam");
+User user2 = new User("Monika", "Adam");
+User user3 = new User("Zuzia", "Adam");
+User user4 = new User("Damian", "Adam");
 
-for (int i = 0; i < letters.Length; i++)
+user1.AddScore(5);
+user1.AddScore(2);
+user2.AddScore(3);
+user2.AddScore(3);
+user2.AddScore(10);
+user3.AddScore(4);
+user3.AddScore(6);
+user3.AddScore(4);
+var result = user1.Result;
+var result2 = user2.Result;
+var result3 = user3.Result; 
+Console.WriteLine(result);
+Console.WriteLine(result2);
+Console.WriteLine(result3);
+
+class User
 {
-    if (letters[i] == '0')
+    private List<int> score = new List<int>();
+    public User(string login, string password)
     {
-        zero++;  
+        this.Login = login;
+        this.Password = password;
+       
     }
-    else if (letters[i] == '1')
-    {
-        one++;
-    }
-    else if (letters[i] == '2')
-    {
-        two++;
-    }
-    else if (letters[i] == '3')
-    {
-        three++;
-    }
-    else if (letters[i] == '4')
-    {
-        four++;
-    }
-    else if (letters[i] == '5')
-    {
-        five++;
-    }
-    else if (letters[i] == '6')
-    {
-        six++;
-    }
-    else if (letters[i] == '7')
-    {
-        seven++;
-    }
-    else if (letters[i] == '8')
-    {
-        eight++;
-    }
-    else 
-    {
-        nine++;
+    public string Login { get; private set; }
+    public string Password { get; private set; }
+    public int Result 
+    { 
+        get
+        {
+            return this.score.Sum();
+        } 
     }
 
+    public void AddScore(int score)
+    {
+        this.score.Add(score);
+    }
 }
-Console.WriteLine("0-->" + zero);
-Console.WriteLine("1-->" + one);
-Console.WriteLine("2-->" + two);
-Console.WriteLine("3-->" + three);
-Console.WriteLine("4-->" + four);
-Console.WriteLine("5-->" + five);
-Console.WriteLine("6-->" + six);
-Console.WriteLine("7-->" + seven);
-Console.WriteLine("8-->" + eight);
-Console.WriteLine("9-->" + nine);
-
 
